779eece3f569d0bf01e0517fe5af3a14a0a66089
phani manda <phanimanda8@gmail.com>
2025-11-22 21:26:09 +0530

    second commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,9 +10,9 @@ import Footer from '@/common/components/shared/footer';
 import { ModeToggle } from '@/common/theme/mode-toggler';
 
 export const metadata: Metadata = {
-  title: 'Maksym Azimov | Portfolio',
+  title: 'Abhishek Parnandhi | Portfolio',
   description:
-    'Check out modern and stylish porfolio of an experienced Web Developer Maksym Azimov! Built using TypeScript, Next.js 14, Framaer Motion and Tailwind CSS.',
+    'Check out modern and stylish porfolio of an experienced video editor and graphic designer! Built using TypeScript, Next.js 14, Framaer Motion and Tailwind CSS.',
 };
 
 const fontMontserrat = FontMontserrat({
